b5f8a441a13810c1500752ce6f4d8e4847cd4209
eggist77 <eggist77@gmail.com>
2020-06-02 21:52:15 +0900

    add new file

---
+++ python/helloworld.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+print("Hello world")
+
+a = 1
+if a == 1:
+    print("A")
+elif a == 2:
+    print("B")
+elif a == 3:
+    print("C")
+else:
+    print("Z")
+print("X")
+
+words = ['one', 'two', 'three']
+
+for word in words:
+    print(word)
