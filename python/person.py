3597f9ad081833d8bac057496bb4304899667aea
eggist77 <eggist77@gmail.com>
2020-11-27 13:58:23 +0900

    modify

--- python/person.py
+++ python/person.py
@@ -8,6 +8,10 @@ class Person:
         self.height = height
         self.weight = weight
 
+    # method
+    def greet(self):
+        print("Hello. My name is", self.name)
+
 
 John = Person("John", 20, 178, 69)
 Smith = Person("Smith", 18, 168, 60)
@@ -17,8 +21,10 @@ print(John.name, "の属性")
 print("年齢：", John.age)
 print("身長：", John.height)
 print("体重：", John.weight)
+John.greet()
 print()
 print(Smith.name, "の属性")
 print("年齢：", Smith.age)
 print("身長：", Smith.height)
 print("体重：", Smith.weight)
+Smith.greet()
